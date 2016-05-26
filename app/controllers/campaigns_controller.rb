66d766bc96ee401580af5e3bf6b2eae29d3ee532
Kleber Brito Moreira <kleberbritomoreira10@gmail.com>
2016-05-26 14:14:58 -0300

    adding changed
    Signed-off-by: Kleber Brito Moreira <kleberbritomoreira10@gmail.com>

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,38 +1,38 @@
 class CampaignsController < ApplicationController
 
 	def new
-		@campaign = Campaign.new
+		@campaign = kleberCampaign.new
 	end
 
 	def create
-		@campaign = Campaign.new(campaign_params)
+		@campaign = kleberCampaign.new(campaign_params)
 		if @campaign.save
 			redirect_to campanhas_path, notice: "Criado com sucesso!"
 		end
 	end
 
 	def show
-    	@campaign = Campaign.find(params[:id])
+    	@campaign = kleberCampaign.find(params[:id])
 	end
 
 	def index
       @user = current_user
-  		@campaigns = Campaign.all
+  		@campaigns = kleberCampaign.all
 	end
 
 	def destroy
-    	@campaign = Campaign.find(params[:id]).destroy
+    	@campaign = kleberCampaign.find(params[:id]).destroy
     	redirect_to campanhas_path, notice: "Campanha deletada."
 	end
 
 	def edit
-    @campaign = Campaign.find params[:id]
+    @campaign = kleberCampaign.find params[:id]
 
       redirect_to "/campaigns" unless @campaign
     end
 
   def update
-    @campaign = Campaign.find params[:id]
+    @campaign = kleberCampaign.find params[:id]
 
     if @campaign.update_attributes(campaign_params)
       redirect_to campaigns_path, :notice => "Campanha foi atualizada"
